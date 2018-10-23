6b0dd7930a92c8816c765551e1bd484ad7b9ba54
Xuan Sang LE <xsang.le@gmail.com>
2018-10-23 19:58:51 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -2,7 +2,7 @@
 export ANTIX_ROOT=~/antix/rootfs/
 export ANTIX_BOOT=~/antix/boot/
 export ANTIX_TOOLS=~/antix/cross-tools/
-export ANTIX_TARGET=arm-linux-musleabihf
+export ANTIX_TARGET=arm-linux-gnueabihf
 export ANTIX_ARCH=armv7-a
 export ANTIX_FLOAT=hard
 export ANTIX_FPU=vfp
