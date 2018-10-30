10ed1df00db2f76cd8aaceae78bef72e2dbecc17
lxsang <xsang.le@gmail.com>
2018-10-30 14:00:41 +0100

    add apk package manager

--- packages/mkapk.sh
+++ packages/mkapk.sh
@@ -12,6 +12,8 @@ else
     git stash
 fi
 git checkout 2.8-stable
+wget https://github.com/lxsang/antix/raw/master/packages/apk-tools-2.8.2.patch
+patch -Np1 -i apk-tools-2.8.2.patch
 make clean
 make -j 8 LUAAPK= DESTDIR=${ANTIX_PKG_BUILD}/apk CROSS_COMPILE="${ANTIX_TARGET}-"  CFLAGS="-Wno-unused-result" static
 make LUAAPK= CROSS_COMPILE="${ANTIX_TARGET}-" DESTDIR=${ANTIX_PKG_BUILD}/apk install
