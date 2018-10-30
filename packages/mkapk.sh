61bd259b19fa603944473eb81259655f209b0da2
lxsang <xsang.le@gmail.com>
2018-10-30 13:57:49 +0100

    add apk package manager

--- packages/mkapk.sh
+++ packages/mkapk.sh
@@ -9,14 +9,12 @@ if [ ! -d "apk-tools" ]; then
     cd apk-tools
 else
     cd apk-tools
-    #git stash
+    git stash
 fi
 git checkout 2.8-stable
 make clean
 make -j 8 LUAAPK= DESTDIR=${ANTIX_PKG_BUILD}/apk CROSS_COMPILE="${ANTIX_TARGET}-"  CFLAGS="-Wno-unused-result" static
 make LUAAPK= CROSS_COMPILE="${ANTIX_TARGET}-" DESTDIR=${ANTIX_PKG_BUILD}/apk install
-#mkdir -pv ${ANTIX_ROOT}/opt/www/{htdocs,database,tmp}
-#cp config.ini.tpl ${ANTIX_ROOT}/opt/www/config.ini
-#chmod u+x ${ANTIX_ROOT}/opt/www/antd
-#cd ${ANTIX_BASE}/source
-#rm -rf ant-http
+cp ${ANTIX_PKG_BUILD}/apk/sbin/apk ${ANTIX_ROOT}/usr/bin
+cd ${ANTIX_BASE}/source
+rm -rf apk-tools
