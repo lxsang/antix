14f38882ba9482844378343981349689c38dedda
lxsang <xsang.le@gmail.com>
2018-10-30 12:28:09 +0100

    fix hdmi output on rpi

--- mkuboot.sh
+++ mkuboot.sh
@@ -14,8 +14,10 @@ make -j 8 ARCH=arm CROSS_COMPILE=${ANTIX_TARGET}-
 cp ${ANTIX_UBIN} ${ANTIX_BOOT}
 if [ "${ANTIX_BOARD}" = "npineo" ]; then
     tty="S0"
+    video=""
 else
     tty="AMA0"
+    video="video=HDMI-A-1:800x600@60D"
 fi
 cat > ${ANTIX_BOOT}/boot.cmd << "EOF"
 # Nano pi neo
@@ -26,7 +28,7 @@ cat > ${ANTIX_BOOT}/boot.cmd << "EOF"
 fatload mmc 0 ${kernel_addr_r} zImage
 fatload mmc 0 ${fdt_addr_r} ${fdtfile}
 EOF
-echo "setenv bootargs console=tty${tty},115200 earlyprintk root=/dev/mmcblk0p2 rootfstype=ext4 rw rootwait panic=10 \${extra}" >> ${ANTIX_BOOT}/boot.cmd
+echo "setenv bootargs console=tty${tty},115200 earlyprintk ${video} root=/dev/mmcblk0p2 rootfstype=ext4 rw rootwait panic=10 \${extra}" >> ${ANTIX_BOOT}/boot.cmd
 echo "bootz \${kernel_addr_r} - \${fdt_addr_r}" >> ${ANTIX_BOOT}/boot.cmd
 
 mkimage -C none -A arm -T script -d ${ANTIX_BOOT}/boot.cmd ${ANTIX_BOOT}/boot.scr
