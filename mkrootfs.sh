482b10b9fe8d2965fe6ae2da39a7b909d856a8f6
lxsang <xsang.le@gmail.com>
2018-10-31 17:22:41 +0100

    fix ncurse bug on htop

--- mkrootfs.sh
+++ mkrootfs.sh
@@ -7,7 +7,13 @@ cd packages
 ./mkzlib.sh
 ./mknetplug.sh
 ./mkdropbear.sh
-./mkbash.sh
+# since busybox comes with
+# sh and ash as shells, we dont really
+# need bash for casual use.
+# It can be install with the apk-tool.
+# it is listed here as an optional
+# option:
+# ./mkbash.sh
 ./mksqlite3.sh
 ./mkopenssl.sh
 ./mkapk.sh
--- packages/mkncurse.sh
+++ packages/mkncurse.sh
@@ -19,10 +19,11 @@ make DESTDIR=${ANTIX_PKG_BUILD}/ncurses install
 cp -v -rf ${ANTIX_PKG_BUILD}/ncurses/lib/* ${ANTIX_TOOLS}/${ANTIX_TARGET}/lib/
 cp -v -rf ${ANTIX_PKG_BUILD}/ncurses/usr/* ${ANTIX_TOOLS}/${ANTIX_TARGET}/usr/
 test ! -L ${ANTIX_TOOLS}/${ANTIX_TARGET}/lib/libncurses.so.6 && ln -s ${ANTIX_TOOLS}/${ANTIX_TARGET}/lib/libncursesw.so.6 ${ANTIX_TOOLS}/${ANTIX_TARGET}/lib/libncurses.so.6
-
+test ! -L ${ANTIX_TOOLS}/${ANTIX_TARGET}/lib/libncurses.so && ln -s ${ANTIX_TOOLS}/${ANTIX_TARGET}/lib/libncursesw.so.6 ${ANTIX_TOOLS}/${ANTIX_TARGET}/lib/libncurses.so
 cp -v -rf ${ANTIX_PKG_BUILD}/ncurses/lib/*.so* ${ANTIX_ROOT}/lib/
 cp -v -rf ${ANTIX_PKG_BUILD}/ncurses/usr/bin/* ${ANTIX_ROOT}/usr/bin/
 cp -v -rf ${ANTIX_PKG_BUILD}/ncurses/usr/share/* ${ANTIX_ROOT}/usr/share/
+test ! -L ${ANTIX_ROOT}/lib/libncurses.so && ln -s /lib/libncursesw.so.6 ${ANTIX_ROOT}/lib/libncurses.so
 test ! -L ${ANTIX_ROOT}/lib/libncurses.so.6 && ln -s /lib/libncursesw.so.6 ${ANTIX_ROOT}/lib/libncurses.so.6
 cd ${ANTIX_BASE}/source
 rm -rf ncurses-6.1 ${ANTIX_PKG_BUILD}/ncurses
