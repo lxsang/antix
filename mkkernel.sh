8f91374c85747cb5877f8fc49ae91af9bb697fe8
lxsang <xsang.le@gmail.com>
2018-11-03 13:07:26 +0100

    spi support for raspberry pi kernel

--- mkkernel.sh
+++ mkkernel.sh
@@ -11,11 +11,11 @@ if [ "${ANTIX_BOARD}" = "npineo" ]; then
     patch -Np1 -i ../sun8i-h3.patch
 fi
 set -e
-make mrproper
+#make mrproper
 #wget https://raw.githubusercontent.com/lxsang/antix/master/nanopi-config-4.9.22
 #mv nanopi-config-4.9.22 .config
-make ARCH=arm CROSS_COMPILE=${ANTIX_TARGET}- ${ANTIX_KERNEL_CONFIG}
-make ARCH=arm CROSS_COMPILE=${ANTIX_TARGET}- menuconfig
+#make ARCH=arm CROSS_COMPILE=${ANTIX_TARGET}- ${ANTIX_KERNEL_CONFIG}
+#make ARCH=arm CROSS_COMPILE=${ANTIX_TARGET}- menuconfig
 make -j 8 ARCH=arm CROSS_COMPILE=${ANTIX_TARGET}-
 make ARCH=arm CROSS_COMPILE=${ANTIX_TARGET}- \
     INSTALL_MOD_PATH=${ANTIX_ROOT} modules_install
@@ -26,9 +26,10 @@ cd ${ANTIX_BASE}/source
 # enable load module
 # raspberry pi 8188eu:
 # enable in Driver/staging
-# enable CFG80211 wireless extension in wireless support
+# enable CFG80211 wireless extension and rfkill in wireless support
 # get firmware from https://github.com/lwfinger/rtl8188eu/raw/master/rtl8188eufw.bin and put in 
 #  /lib/firmware/rtlwifi/
+# to enable spi, use mode spi device should be enabled on SPI support
 
 # For nanopineo
 # enable sunxi8-emac driver in Driver/network driver/ethernet
