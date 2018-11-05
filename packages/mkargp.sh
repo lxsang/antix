35780008923a6d2c36ccc589cefe875e4bd9af2f
lxsang <xsang.le@gmail.com>
2018-11-06 00:01:09 +0100

    add libv4l

---
+++ packages/mkargp.sh
@@ -0,0 +1,17 @@
+
+#! /bin/bash
+set -e
+. ../env.sh
+cd ${ANTIX_BASE}/source
+if [ ! -d "argp-standalone" ]; then
+    # download it
+    git clone https://github.com/jahrome/argp-standalone
+fi
+
+cd argp-standalone
+./configure --host=${ANTIX_TARGET} 
+make DESTDIR=${ANTIX_PKG_BUILD}/argp install
+#cp -rf ${ANTIX_PKG_BUILD}/libjpeg/usr/local/* ${ANTIX_TOOLS}/${ANTIX_TARGET}/
+# install shared library to the toolchain
+cd ${ANTIX_BASE}/source
+#${ANTIX_PKG_BUILD}/libjpeg
