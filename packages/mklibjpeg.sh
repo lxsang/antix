079ea268b5d553aa8191c524627b00fdd6a83c76
lxsang <xsang.le@gmail.com>
2018-11-05 22:55:00 +0100

    fix

--- packages/mklibjpeg.sh
+++ packages/mklibjpeg.sh
@@ -9,9 +9,10 @@ fi
 tar xvf jpegsrc.v9c.tar.gz
 cd jpeg-9c
 ./configure --host=${ANTIX_TARGET} \
-   CC=${ANTIX_TARGET}-cc
+   CC=${ANTIX_TARGET}-gcc
 make -j 8
 make DESTDIR=${ANTIX_PKG_BUILD}/libjpeg install
+cp ${ANTIX_PKG_BUILD}/libjpeg/usr/local/* ${ANTIX_TOOLS}/${ANTIX_TARGET}/
 # install shared library to the toolchain
 cd ${ANTIX_BASE}/source
-rm -rf jpeg-9c #${ANTIX_PKG_BUILD}/ncurses
+rm -rf jpeg-9c ${ANTIX_PKG_BUILD}/libjpeg
