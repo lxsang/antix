079ab0af31610e32ed12283dbb93bdcdc0eda1b8
lxsang <xsang.le@gmail.com>
2018-11-05 23:46:26 +0100

    add libv4l

--- packages/mklibv4l.sh
+++ packages/mklibv4l.sh
@@ -2,14 +2,15 @@
 set -e
 . ../env.sh
 cd ${ANTIX_BASE}/source
-if [ ! -d "libv4l" ]; then
+if [ ! -f "v4l-utils-1.16.1.tar.bz2" ]; then
     # download it
-    git clone https://github.com/philips/libv4l
+    wget https://linuxtv.org/downloads/v4l-utils/v4l-utils-1.16.1.tar.bz2
 fi
-cd libv4l
-CC=${ANTIX_TARGET}-gcc make -j 8
+tar xvf v4l-utils-1.16.1.tar.bz2
+cd v4l-utils-1.16.1
+./configure --host=${ANTIX_TARGET} 
 make DESTDIR=${ANTIX_PKG_BUILD}/libv4l install
 #cp -rf ${ANTIX_PKG_BUILD}/libjpeg/usr/local/* ${ANTIX_TOOLS}/${ANTIX_TARGET}/
 # install shared library to the toolchain
 cd ${ANTIX_BASE}/source
-rm -rf libv4l #${ANTIX_PKG_BUILD}/libjpeg
+rm -rf v4l-utils-1.16.1 #${ANTIX_PKG_BUILD}/libjpeg
