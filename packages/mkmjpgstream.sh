0281cd71404289ffac104f5555dd23bd1280d9d4
lxsang <xsang.le@gmail.com>
2018-11-06 00:50:30 +0100

    add mjpg

--- packages/mkmjpgstream.sh
+++ packages/mkmjpgstream.sh
@@ -11,6 +11,13 @@ cd mjpg-streamer
 make clean
 sed  -i -E "s/CC = gcc/CC=${ANTIX_TARGET}-gcc/" Makefile
 sed  -i -E "s%DESTDIR = /usr/local%DESTDIR =${ANTIX_PKG_BUILD}/mjpg-stream%" Makefile
+for file in plugins; do
+    if [ -d "plugins/$file" ]; then
+        if [ -f "plugins/$file/Makefile"]; then
+            sed  -i -E "s/CC = gcc/CC=${ANTIX_TARGET}-gcc/" "plugins/$file/Makefile"
+        fi
+    fi
+done
 set +e
 ln -s ${ANTIX_TOOLS}/${ANTIX_TARGET}/include/linux/videodev2.h ${ANTIX_TOOLS}/${ANTIX_TARGET}/include/linux/videodev.h
 set -e
