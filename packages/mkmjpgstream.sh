00225ebbe7b69d21f9f98235642f1105270afae4
lxsang <xsang.le@gmail.com>
2018-11-06 00:30:11 +0100

    add mjpg

---
+++ packages/mkmjpgstream.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+set -e
+. ../env.sh
+cd ${ANTIX_BASE}/source
+if [ ! -f "mjpg-streamer.tar.gz" ]; then
+    # download it
+    wget http://terzo.acmesystems.it/download/webcam/mjpg-streamer.tar.gz
+fi
+tar xvf mjpg-streamer.tar.gz
+cd mjpg-streamer
+CC=${ANTIX_TARGET}-gcc make -j 8
+make DESTDIR=${ANTIX_PKG_BUILD}/mjpg-stream install
+#cp -rf ${ANTIX_PKG_BUILD}/libv4l/usr/local/* ${ANTIX_TOOLS}/${ANTIX_TARGET}/
+# install shared library to the toolchain
+cd ${ANTIX_BASE}/source
+rm -rf mjpg-streamer #${ANTIX_PKG_BUILD}/libv4l
