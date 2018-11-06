056dc5f449a8beb37f7ac62fb996b75d2da5c1f2
lxsang <xsang.le@gmail.com>
2018-11-06 01:40:18 +0100

    add mjpg

---
+++ packages/mkuvc-streamer.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+set -e
+. ../env.sh
+cd ${ANTIX_BASE}/source
+if [ ! -d "uvc-streamer" ]; then
+    # download it
+    git clone https://github.com/bsapundzhiev/uvc-streamer
+fi
+
+cd uvc-streamer
+./configure --host=${ANTIX_TARGET} CC=${ANTIX_TARGET}-gcc
+CC=${ANTIX_TARGET}-gcc  make -j 8
+CC=${ANTIX_TARGET}-gcc make DESTDIR=${ANTIX_PKG_BUILD}/uvc-streamer install
+#cp -rf ${ANTIX_PKG_BUILD}/argp/usr/local/* ${ANTIX_TOOLS}/${ANTIX_TARGET}/
+# install shared library to the toolchain
+cd ${ANTIX_BASE}/source
+rm -rf uvc-streamer 
+#${ANTIX_PKG_BUILD}/argp
