e1c8a7e03d64d3d1dc62a6a69241527199d1e3cd
lxsang <xsang.le@gmail.com>
2018-11-06 01:30:31 +0100

    fix stuff

--- packages/mkmjpgstream.sh
+++ packages/mkmjpgstream.sh
@@ -10,14 +10,18 @@ tar xvf mjpg-streamer.tar.gz
 cd mjpg-streamer
 make clean
 sed  -i -E "s/CC = gcc/CC=${ANTIX_TARGET}-gcc/" Makefile
+sed -i -E "s/#include <syslog.h>/#include <syslog.h>\n#include <pthread.h>/" plugins/input.h
+sed -i -E "s/PLUGINS \+= input_gspcav1.so/# PLUGINS \+= input_gspcav1.so/" Makefile
 sed  -i -E "s%DESTDIR = /usr/local%DESTDIR =${ANTIX_PKG_BUILD}/mjpg-stream%" Makefile
-for file in plugins; do
-    if [ -d "plugins/$file" ]; then
-        if [ -f "plugins/$file/Makefile"]; then
-            sed  -i -E "s/CC = gcc/CC=${ANTIX_TARGET}-gcc/" "plugins/$file/Makefile"
+for file in plugins/*; do
+    if [ -d "$file" ]; then
+        if [ -f "$file/Makefile" ]; then
+            echo $file
+            sed  -i -E "s/CC = gcc/CC=${ANTIX_TARGET}-gcc/" "$file/Makefile"
         fi
     fi
 done
+mkdir -p ${ANTIX_PKG_BUILD}/mjpg-stream/{bin,lib}
 set +e
 ln -s ${ANTIX_TOOLS}/${ANTIX_TARGET}/include/linux/videodev2.h ${ANTIX_TOOLS}/${ANTIX_TARGET}/include/linux/videodev.h
 set -e
