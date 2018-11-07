418770f956807475f141297a19861ff66b7511f5
lxsang <xsang.le@gmail.com>
2018-11-07 11:23:38 +0100

    fix stuff

--- packages/mkwirelesstool.sh
+++ packages/mkwirelesstool.sh
@@ -23,16 +23,3 @@ cp ${ANTIX_PKG_BUILD}/wireless/usr/sbin/* ${ANTIX_ROOT}/usr/sbin
 cd ${ANTIX_BASE}/source
 rm -rf wireless_tools.29
 # iw tools is only allow wep connection
-# to connect to wpa wifi, we need wpa_supplicant
-if [ ! -f "wpa_supplicant-2.6.tar.gz" ]; then
-    # download it
-    wget http://w1.fi/releases/wpa_supplicant-2.6.tar.gz
-fi
-tar xvf wpa_supplicant-2.6.tar.gz
-cd wpa_supplicant-2.6/wpa_supplicant
-# cross compile it
-cp defconfig .config
-make CC=${ANTIX_TARGET}-gcc
-make install DESTDIR=${ANTIX_PKG_BUILD}/wireless/usr
-cd ${ANTIX_BASE}/source
-rm -rf  ${ANTIX_PKG_BUILD}/wireless wpa_supplicant-2.6
