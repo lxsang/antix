38ffa241b4c810e4fede6bb1ad347b62359821f4
lxsang <xsang.le@gmail.com>
2018-11-18 00:56:00 +0100

    add more package

--- packages/mkicu.sh
+++ packages/mkicu.sh
@@ -2,7 +2,7 @@
 
 set -e
 . ../env.sh
-. ../toolchain.sh
+#. ../toolchain.sh
 dir="icu4c-63_1-src"
 cd ${ANTIX_BASE}/source
 if [ ! -f "${dir}.tgz" ]; then
@@ -10,17 +10,29 @@ if [ ! -f "${dir}.tgz" ]; then
     wget http://download.icu-project.org/files/icu4c/63.1/icu4c-63_1-src.tgz
 fi
 tar xvf ${dir}.tgz
-cd ${dir}
+cd icu/source
+if [ ! -d "${ANTIX_BASE}/source/icu/source/host-build/out" ]; then
+    mkdir -p host-build
+    cd host-build
+    ../configure --prefix=${ANTIX_BASE}/source/icu/source/host-build/out\
+        --disable-samples --disable-tests
+    make -j 8
+    make install
+    cd ../
+fi
+mkdir -p cross-build
+cd cross-build
 # configure cross platform
-./configure --prefix=${ANTIX_PKG_BUILD}/icu\
+../configure --prefix=${ANTIX_TOOLS}/${ANTIX_TARGET}\
     --build=${ANTIX_HOST}\
     --host=${ANTIX_TARGET}\
-    --disable-samples --disable-tests
+    --disable-samples --disable-tests\
+    --with-cross-build=${ANTIX_BASE}/source/icu/source/host-build
    
 make -j8
 make install
 
-#cp -avrf ${ANTIX_TOOLS}/${ANTIX_TARGET}/lib/libharfbuzz*.so* ${ANTIX_ROOT}/usr/lib
+cp -avrf ${ANTIX_TOOLS}/${ANTIX_TARGET}/lib/libicu*.so* ${ANTIX_ROOT}/usr/lib
 
 cd ${ANTIX_BASE}/source
-rm -rf  ${dir}
+rm -rf  icu
