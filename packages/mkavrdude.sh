ad62fca4f5e19da44ea9980178b235db40d7f3f2
lxsang <xsang.le@gmail.com>
2019-04-03 19:10:38 +0200

    add avrdude

---
+++ packages/mkavrdude.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+# build wiring pi library
+# for raspberry Pi flavor devices
+set -e
+. ../env.sh
+. ../toolchain.sh
+cd ${ANTIX_BASE}/source
+if [ ! -d "avrdude-6.3" ]; then
+    # download it
+    git clone https://github.com/arie-g/avrdude-6.3
+    
+fi
+if [ ! -f "avrdude6.3.patch" ]; then
+    wget https://github.com/lxsang/antix/raw/master/avrdude6.3.patch
+fi
+
+cd avrdude-6.3
+# patch it
+ patch -Np1 -i avrdude6.3.patch 
+# now configure the code
+./bootstrap
+./configure \
+    --build=${ANTIX_HOST}\
+    --host=${ANTIX_TARGET}\
+    --enable-linuxgpio
+exit 0
+make clean
+make
+cp -av avrdude ${ANTIX_ROOT}/usr/bin/
+mkdir -p ${ANTIX_ROOT}/usr/local/etc/
+cp -av avrdude.conf ${ANTIX_ROOT}/usr/local/etc/
+exit 0
