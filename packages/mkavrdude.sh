9f877bed405a3580e1cbfce7fda88a2040b426f1
lxsang <xsang.le@gmail.com>
2019-04-04 18:49:37 +0200

    add resettable programmer

--- packages/mkavrdude.sh
+++ packages/mkavrdude.sh
@@ -22,4 +22,41 @@ make
 cp -av avrdude ${ANTIX_ROOT}/usr/bin/
 mkdir -p ${ANTIX_ROOT}/usr/local/etc/
 cp -av avrdude.conf ${ANTIX_ROOT}/usr/local/etc/
+# create the programmer for raspberry Pi
+if [ "${ANTIX_BOARD}" = "npineo" ]; then
+    echo "[Skip generating resettable programmer]"
+else
+cat > ${ANTIX_ROOT}/usr/bin/fwflash << "EOF"
+#! /bin/sh
+if [ "$#" -ne 1 ]; then
+	echo "usage: $0 hex_file"
+	exit 0
+fi
+rst="18"
+delay=1
+echo $rst > /sys/class/gpio/unexport
+ 
+# then run the programmer
+echo "Running programmer"
+avrdude  -v -p atmega328p -c arduino -P /dev/ttyAMA0 -b115200 -D -Uflash:w:$1:i & 
+# reset the chip
+echo "Resetting the chip"
+sleep $delay
+echo $rst > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio$rst/direction
+echo "1"> /sys/class/gpio/gpio$rst/value
+sleep $delay
+echo "0" > /sys/class/gpio/gpio$rst/value
+#wait for the background command finish
+wait $!
+# run the program
+echo "Run the program"
+echo "1"> /sys/class/gpio/gpio$rst/value
+sleep $delay
+echo "0"> /sys/class/gpio/gpio$rst/value
+sleep $delay
+echo $rst > /sys/class/gpio/unexport
+EOF
+chmod a+x ${ANTIX_ROOT}/usr/bin/fwflash
+fi
 exit 0
